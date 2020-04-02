8399b576e9456136bface68adb5ae3110fe47aa4
Saksham27 <geekysaksham27@gmail.com>
2020-04-02 17:58:31 +0530

    Added: implemented cash counter for bank

---
+++ CashCounter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BankingCashCounter
+{
+    class CashCounter
+    {
+        private const int STARTING_BALANCE = 5000;
+        private int balance;
+        public CashCounter()
+        {
+            this.balance = STARTING_BALANCE;
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
